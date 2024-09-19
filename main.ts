39784dd6fa20541d1c13030922394e416a1e90e3
Iqra naz <162154619+Iqranaz993@users.noreply.github.com>
2024-09-19 19:35:43 +0500

    this is the simple cli base oop project

---
+++ main.ts
@@ -0,0 +1,62 @@
+#! /user/bin/env node
+
+import inquirer from "inquirer";
+
+class Student {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+class Person {
+    students: Student[] = [];
+    addStudent(obj: Student) {
+        this.students.push(obj);
+        console.log(`New student added: ${obj.name}`);
+        console.log("Current student list:", this.students.map(s => s.name).join(", "));
+    }
+}
+
+const persons = new Person();
+
+const programStart = async () => {
+    let exitProgram = false;
+
+    while (!exitProgram) {
+        console.log("Welcome!");
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "Whom would you like to interact with?",
+            choices: ["staff", "student", "exit"]
+        });
+
+        if (ans.select === "staff") {
+            console.log("You approach the staff room. Feel free to ask any questions:");
+        } 
+        else if (ans.select === "student") {
+            const studentAns = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter the student's name you wish to engage with",
+            });
+
+            const student = persons.students.find(val => val.name === studentAns.student);
+            if (!student) {
+                const newStudent = new Student(studentAns.student);
+                persons.addStudent(newStudent);
+                console.log(`Hello, I am ${newStudent.name}. Nice to meet you!`);
+            } 
+            else {
+                console.log(`Hello, I am ${student.name}. Nice to see you again!`);
+            }
+        } 
+        else if (ans.select === "exit") {
+            console.log("Exiting the program...");
+            exitProgram = true;
+        }
+    }
+};
+
+programStart();
